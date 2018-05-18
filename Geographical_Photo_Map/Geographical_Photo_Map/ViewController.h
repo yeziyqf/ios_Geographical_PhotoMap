3f6fa2c10b89d94e6e8a002bfcce9a156e46b38b
qye <qye@qyes-MacBook-Pro.local>
2018-05-17 17:27:18 -0700

    Adding user real-time location service and show on map.

--- Geographical_Photo_Map/Geographical_Photo_Map/ViewController.h
+++ Geographical_Photo_Map/Geographical_Photo_Map/ViewController.h
@@ -7,8 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface ViewController : UIViewController
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UILabel *labelLatitude;
+@property (weak, nonatomic) IBOutlet UILabel *labelLongitude;
+@property (weak, nonatomic) IBOutlet UILabel *labelAltitude;
 
 
 @end
--- Geographical_Photo_Map/Geographical_Photo_Map/ViewController.m
+++ Geographical_Photo_Map/Geographical_Photo_Map/ViewController.m
@@ -5,10 +5,13 @@
 //  Created by qye on 5/17/18.
 //  Copyright © 2018 qye. All rights reserved.
 //
+#define METERS_MILE 1609.344
+#define METERS_FEET 3.28084
 
 #import "ViewController.h"
 
 @interface ViewController ()
+<CLLocationManagerDelegate>
 
 @end
 
@@ -17,12 +20,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [[self mapView] setShowsUserLocation:YES];
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    
+    [[self locationManager] setDelegate:self];
+    
+    
+    // we have to setup the location manager with permission in later iOS versions
+    if ([[self locationManager] respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        [[self locationManager] requestWhenInUseAuthorization];
+    }
+    
+    [[self locationManager] setDesiredAccuracy:kCLLocationAccuracyBest];
+    [[self locationManager] startUpdatingLocation];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *location = locations.lastObject;
+    [[self labelLatitude] setText:[NSString stringWithFormat:@"%.6f",
+                                   location.coordinate.latitude]];
+    [[self labelLongitude] setText:[NSString stringWithFormat:@"%.6f",
+                                    location.coordinate.longitude]];
+    [[self labelAltitude] setText:[NSString stringWithFormat:@"%.2f feet",
+                                   location.altitude*METERS_FEET]];
 }
 
 
